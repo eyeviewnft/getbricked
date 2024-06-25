13ddd165ac37e3f86350af4c80b44f36264878be
eyeviewnft <158103746+eyeviewnft@users.noreply.github.com>
2024-06-25 17:29:10 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+    output: "export",
 };
 
 export default nextConfig;
