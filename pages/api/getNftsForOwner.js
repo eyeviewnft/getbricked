502beeeea31184a8aeb5031b336f17176c32d578
eyeviewnft <158103746+eyeviewnft@users.noreply.github.com>
2024-06-25 18:54:48 +0200

    Update getNftsForOwner.js

--- pages/api/getNftsForOwner.js
+++ pages/api/getNftsForOwner.js
@@ -31,15 +31,13 @@ export default async function handler(req, res) {
 				contract: contract.address,
 				symbol: contract.symbol,
 				collectionName: contract.name,
-				media: Array.isArray(media) && media[0]
-            ? media[0]
-            : "https://via.placeholder.com/500",
+				media: media.length > 0 ? media[0] : "https://via.placeholder.com/500",
 				verified: contract.openSea?.safelistRequestStatus,
 				tokenType,
 				tokenId,
 				title,
 				description,
-				format: Array.isArray(media) && media[0]?.format ? media[0]?.format : "png",
+				format: media.length > 0 ?.format ? media[0]?.format : "png",
 			};
 		});
 		if (excludeFilter) {
